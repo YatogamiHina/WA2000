387115a309c7388d9c41f1c9207a932f5ae812d7
Yotsuha <32481230+Yotsuha@users.noreply.github.com>
2018-11-16 06:10:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -764,12 +764,14 @@ function YabasoReply(inputStr) {
     let Answer = rplyArr[Dice(rplyArr.length)-1];
 	let temp;
     if(Answer.match('選') != null||Answer.match('決定') != null||Answer.match('挑') != null||Answer.match('WA2000') != null) {
-
+   
+    rplyArr = [' '];
+    
     temp = rplyArr[Dice(rplyArr.length)-1];
 	    
-	   // while(temp.match('WA2000') != null || temp.match('選') != null || temp.match('挑') != null || temp.match('決定') != null){
-		//    temp = rplyArr[Dice(rplyArr.length)-1];
-	    //}
+	    while(temp.match('WA2000') != null || temp.match('選') != null || temp.match('挑') != null || temp.match('決定') != null){
+		    temp = rplyArr[Dice(rplyArr.length)-1];
+	    }
 	Answer = temp;    
     }
     return '我想想喔……我覺得，' + Answer + '。';
